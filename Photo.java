2389cdcf855cdb1535676cca565a7912258d2dc3
Cedric.C <kmuced@gmail.com>
2022-08-25 10:39:41 +0200

    ts to java

---
+++ Photo.java
@@ -0,0 +1,6 @@
+public class Photo {
+    public String url;
+    public String filename;
+    public Number width;
+    public Number height;
+}
---
+++ User.java
@@ -0,0 +1,8 @@
+import java.sql.Date;
+
+public class User {
+    public Date birthdate;
+    public Number size;
+    public Photo[] photo;  
+    public Address address; 
+}
